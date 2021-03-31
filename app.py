fd836479d1473cb14b961ba29deb6675fe0978d8
sadlerb <sadlerb22@gmail.com>
2021-03-31 15:42:40 -0500

    Flask Starter Added

---
+++ app.py
@@ -0,0 +1,25 @@
+"""
+Flask Documentation:     http://flask.pocoo.org/docs/
+Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
+Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+This file creates your application.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+'''
+# Routing for your application.
+# Put your routes below this comment
+'''
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run()
